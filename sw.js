31fd31ec97b11a8920e8823844615ce3c7226d2c
shun-program <80468776+shun-program@users.noreply.github.com>
2023-10-11 17:18:01 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './pages/onlineJudgment.html',
+  './pages/menue.html',
+  './pages/a.html',
+  './pages/b.html',
+  './css/style.css',
+  './js/script.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});

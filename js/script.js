75667ce34edeff67b632d270dd8b2bc73f05d618
shun-program <80468776+shun-program@users.noreply.github.com>
2023-10-12 14:55:20 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ let isOnline;
 isOnline = navigator.onLine;
 
 if(isOnline === true){
-  document.querySelector(".results").innerHTML = "インターネットに接続中です";
+  // document.querySelector(".results").innerHTML = "インターネットに接続中です";
   document.getElementById("offline").style.display = "none";
 
 }else{
-  document.querySelector(".results").innerHTML = "インターネットから切断されました";
+  // document.querySelector(".results").innerHTML = "インターネットから切断されました";
   document.getElementById("online").style.display = "none";
 }
